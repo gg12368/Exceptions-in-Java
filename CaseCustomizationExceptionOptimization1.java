8411e0ce6b9dac17c51b4be19840943a2e0019eb
gg12368 <56186550+gg12368@users.noreply.github.com>
2020-02-04 19:36:28 +0800

    Create CaseCustomizationExceptionOptimization1.java
    案例自定义异常优化部分1

---
+++ CaseCustomizationExceptionOptimization1.java
@@ -0,0 +1,10 @@
+class UserError extends Exception { 
+    public UserError(String message) { 
+        super(message); 
+    } 
+}
+class PasswordError extends Exception { 
+    public PasswordError(String message) { 
+    super(message); 
+    } 
+}
