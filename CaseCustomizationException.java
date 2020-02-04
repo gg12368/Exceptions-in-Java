707d2a8c06ecd01a638bf48978f00421a23cb34b
gg12368 <56186550+gg12368@users.noreply.github.com>
2020-02-04 19:33:11 +0800

    Create CaseCustomizationException.java
    案例自定义异常

---
+++ CaseCustomizationException.java
@@ -0,0 +1,15 @@
+public class Test { 
+    private static String userName = "admin"; 
+    private static String password = "123456"; 
+public static void main(String[] args) { 
+    login("admin", "123456"); 
+}
+public static void login(String userName, String password) { 
+    if (!Test.userName.equals(userName)) { 
+        // TODO 处理用户名错误 
+    }if (!Test.password.equals(password)) {
+        // TODO 处理密码错误 
+    }
+    System.out.println("登陆成功"); 
+    } 
+}
